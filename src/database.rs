d2041da329b22ad7821389277e4af72da5fc8519
Adrian Borup <avborup@gmail.com>
2020-12-15 19:39:59 +0100

    Split database queries into own functions

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,5 @@
 use crate::schema::types::{
-    Cocktail, Ingredient, IngredientType, NewCocktail, NewIngredientType, User,
+    Cocktail, Ingredient, IngredientType, NewCocktail, NewIngredient, NewIngredientType, User,
 };
 use chrono::Utc;
 use sqlx::PgPool;
@@ -9,13 +9,7 @@ pub async fn insert_new_cocktail(
     db: &PgPool,
     new_cocktail: NewCocktail,
 ) -> Result<Cocktail, sqlx::error::Error> {
-    let author = sqlx::query_as!(
-        User,
-        "SELECT id, name FROM users WHERE id = $1",
-        new_cocktail.author_id,
-    )
-    .fetch_one(db)
-    .await?;
+    let author = get_user(db, new_cocktail.author_id).await?;
 
     let id = Uuid::new_v4();
     let date_added = Utc::now();
@@ -35,16 +29,34 @@ pub async fn insert_new_cocktail(
     .fetch_one(db)
     .await?;
 
-    let mut cocktail = Cocktail {
+    let ingredients = insert_new_ingredients(db, id, &new_cocktail.ingredients).await?;
+
+    let cocktail = Cocktail {
         id: db_output.id,
         name: db_output.name,
         author,
         source: db_output.source,
         date_added: db_output.date_added,
-        ingredients: Vec::new(),
+        ingredients,
     };
 
-    for new_ingredient in new_cocktail.ingredients {
+    Ok(cocktail)
+}
+
+async fn get_user(db: &PgPool, user_id: Uuid) -> Result<User, sqlx::error::Error> {
+    sqlx::query_as!(User, "SELECT id, name FROM users WHERE id = $1", user_id,)
+        .fetch_one(db)
+        .await
+}
+
+async fn insert_new_ingredients(
+    db: &PgPool,
+    cocktail_id: Uuid,
+    new_ingredients: &[NewIngredient],
+) -> Result<Vec<Ingredient>, sqlx::error::Error> {
+    let mut ingredients = Vec::with_capacity(new_ingredients.len());
+
+    for new_ingredient in new_ingredients {
         let ingredient_type = sqlx::query_as!(
             IngredientType,
             "SELECT id, label FROM ingredient_types WHERE id = $1",
@@ -59,7 +71,7 @@ pub async fn insert_new_cocktail(
             VALUES ($1, $2, $3, $4, $5)
             RETURNING label, amount, unit
             ",
-            id,
+            cocktail_id,
             new_ingredient.label,
             new_ingredient.amount,
             new_ingredient.unit,
@@ -75,10 +87,10 @@ pub async fn insert_new_cocktail(
             ingredient_type,
         };
 
-        cocktail.ingredients.push(ingredient);
+        ingredients.push(ingredient);
     }
 
-    Ok(cocktail)
+    Ok(ingredients)
 }
 
 pub async fn insert_new_ingredient_type(
