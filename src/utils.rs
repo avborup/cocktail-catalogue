b5c42a22a7a8300db113473ce0b36f5f5e818139
Adrian Borup <adrian@borup.com>
2023-07-23 18:10:03 +0200

    Remove files that were accidentally restored

--- src/utils.rs
+++
@@ -1,7 +0,0 @@
-use std::time::{SystemTime, SystemTimeError};
-
-pub fn get_cur_time_unix() -> Result<u64, SystemTimeError> {
-    let dur = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
-    Ok(dur.as_secs())
-}
-
